624a1a6de72c74daffbb31f2567b8c60ffd9d8dd
Karolina Pytlak <karolina.py@modulesgarden.com>
2020-03-24 18:33:09 +0100

    'all'

--- 02-JSCoreConcepts/address.js
+++ 02-JSCoreConcepts/address.js
@@ -21,7 +21,7 @@ var adress1 = title + ' ' + name + ' ' + surname + "\n"
              + zip + ' ' + city +"\n"
              + country;
 
-var adress2 = `${title} ${name} ${surname} \n ul. ${street} \n ${zip} ${city} \n ${country}`;
+var adress2 = `${title} ${name} ${surname} \nul.${street} \n${zip} ${city} \n${country}`;
 
 console.log(adress1);
 console.log(adress2);
